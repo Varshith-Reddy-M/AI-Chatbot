d4d14b77ac6fbbfeba44718222b1e637856199dd
Varshith Reddy <varshithreddymutra2006@gmail.com>
2026-04-05 13:15:12 +0530

    updated commit

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import os
 import time
 import streamlit as st
 from openai import OpenAI
-client = OpenAI(api_key="YOUR_API_KEY",base_url="https://openrouter.ai/api/v1")
+client = OpenAI(api_key=os.getenv("API_KEY"),base_url="https://openrouter.ai/api/v1")
 st.title("DevSenseAI")
 
 if "chats" not in st.session_state:
